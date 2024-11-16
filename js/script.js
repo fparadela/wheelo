215e8ebf7bec39dade0bc0a002cd125e0a75dd8e
Felipe Paradela <felipeparadela@MacBook-Pro-van-Yelle.local>
2024-11-16 13:48:18 +0100

    added activated menu bar with javascript

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-console.log("teste'");
+const links = document.querySelectorAll('.header-menu a');
+
+function activateLink(link){
+    const url = window.location.href;
+    const href = link.href;
+
+    if(url.includes(href)) {
+        link.classList.add("activate");
+        console.log();
+
+    }
+}
+
+links.forEach(activateLink)
